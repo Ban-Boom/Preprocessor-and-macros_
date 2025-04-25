1920d52160beed9c814d88a58ba4f21c212fbf07
Bronislav <bronkyr59@gmail.com>
2025-04-25 13:03:18 +0300

    Macro

---
+++ Preprocessing directives/Preprocessing directives/Preprocessing directives.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+
+#define MODE 1
+
+#ifndef MODE
+#error "You must define the constant MODE"
+#endif
+
+#if MODE == 0
+
+int main() {
+    std::cout << "Running in training mode" << std::endl;
+    return 0;
+}
+
+#elif MODE == 1
+
+int add(int a, int b) {
+    return a + b;
+}
+
+int main() {
+    std::cout << "Running in combat mode" << std::endl;
+    int x, y;
+    std::cout << "Enter number 1: ";
+    std::cin >> x;
+    std::cout << "Enter number 2: ";
+    std::cin >> y;
+    std::cout << "Sum result: " << add(x, y) << std::endl;
+    return 0;
+}
+
+#else
+
+int main() {
+    std::cout << "Unknown mode. Exiting." << std::endl;
+    return 0;
+}
+
+#endif
